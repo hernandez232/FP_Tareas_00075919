f6dcbe4893cc2282a9c6129a1d08a53a36324483
hernandez232 <54686582+hernandez232@users.noreply.github.com>
2019-11-09 19:05:04 -0600

    Create Suma.cpp

---
+++ Suma.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int a=0, b=0, suma=0;
+    cout<<"Ingrese dos numeros:"<<endl;
+    cin>>a>>b;
+    suma=a+b;
+    cout<<"La suma de los numeros ingresados es: "<<suma<<endl;
+    
+    return 0;
+}

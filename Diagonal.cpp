0f45c9ea6251868285db6689c57e6727820dcc0d
hernandez232 <54686582+hernandez232@users.noreply.github.com>
2019-11-09 19:09:51 -0600

    Create Diagonal.cpp

---
+++ Diagonal.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int f, c, matriz[10][10];
+
+    cout << "Ingrese el numero de filas de la matriz: "; cin >> f;
+    cout << "Ingrese el numero de columnas de la matriz: "; cin >> c;
+    cout << endl;
+    
+    for (int i=0; i<f; i++){
+        for (int j=0; j<c; j++){
+            cout << "Ingrese el numero de la posicion ["<<i<<"]["<<j<<"]: ";
+            cin >> matriz[i][j];
+        }
+    }
+
+    cout << endl;
+    cout << "La diagonal principal de la matriz es: ";
+    for (int i=0; i<f; i++){
+        for (int j=0; j<c; j++){
+            if (i == j){
+                cout << matriz [i][j] << " ";
+            }
+        }
+    }    
+
+    return 0;
+}

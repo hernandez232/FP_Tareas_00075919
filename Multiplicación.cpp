814b80409507c9396e5451cc84193fe436d142c0
hernandez232 <54686582+hernandez232@users.noreply.github.com>
2019-11-09 19:03:06 -0600

    Create Multiplicación.cpp

---
+++ Multiplicación.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int a=0, b=0, multi=0;
+    cout<<"Ingrese dos numeros:"<<endl;
+    cin>>a>>b;
+    multi=a*b;
+    cout<<"La multiplicacion de los numeros ingresados es: "<<multi<<endl;
+    
+    return 0;
+}

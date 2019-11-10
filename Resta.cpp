7f7ecbbd98a5ca33e0a7209ade1825355e863b33
hernandez232 <54686582+hernandez232@users.noreply.github.com>
2019-11-09 19:03:55 -0600

    Create Resta.cpp

---
+++ Resta.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int a=0, b=0, resta=0;
+    cout<<"Ingrese dos numeros:"<<endl;
+    cin>>a>>b;
+    resta=a-b;
+    cout<<"La resta de los numeros ingresados es: "<<resta<<endl;
+    
+    return 0;
+}

fbb57fd3b0a9f8595636468099d472285917a023
hernandez232 <54686582+hernandez232@users.noreply.github.com>
2019-11-09 19:00:17 -0600

    Create División.cpp

---
+++ División.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    float a=0, b=0, div=0;
+    cout<<"Ingrese dos numeros:"<<endl;
+    cin>>a>>b;
+    div=a/b;
+    cout<<"La division de los numeros ingresados es: "<<div<<endl;
+    
+    return 0;
+}
